c641551c568a553213dc5b622f2e84eb0e9d36f3
shilpasree209 <shilpasree209@gmail.com>
2024-10-14 12:55:09 -0500

    Styles Added
    Changes to styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className='main-container'>
       <h1>Generative AI Web App</h1>
+      <div className='form-container'>
+        <div>
+          <label>Enter Query: </label>
+          <input type='text' className='query-input' placeholder='Enter the query'></input>
+        </div>
+
+      </div>
     </div>
   )
 }
