04218dcb1159e740b192282abda20e0adf4bce44
shilpasree209 <shilpasree209@gmail.com>
2024-10-14 13:11:07 -0500

    Form Created

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,32 @@ function App() {
       <div className='form-container'>
         <div>
           <label>Enter Query: </label>
-          <input type='text' className='query-input' placeholder='Enter the query'></input>
+          <input type='text' 
+            className='query-input' 
+            placeholder='Enter Query'>
+            
+          </input>
+        </div>
+        <div>
+          <label>No of Questions: </label>
+          <input type='range' 
+                  min={1} 
+                  max={10} 
+                  className='question-input' >
+          </input>
+        </div>
+        <div>
+          <label>Difficulty: </label>
+          <select className='difficult-input'>
+            <option value='easy'>Easy</option>
+            <option value='medium'>Medium</option>
+            <option value='hard'>Hard</option>
+          </select>
+          {/* <input type='text' 
+                className='query-input' 
+                placeholder='Enter Query'>
+                
+          </input> */}
         </div>
 
       </div>
